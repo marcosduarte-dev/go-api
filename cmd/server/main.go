e00e18b80b4f7f6849d4223fa5ae0aded7e3f9bf
Marcos Duarte <pe.marcos30@gmail.com>
2023-12-14 12:24:44 -0300

    feat: :sparkles: Creating handlers and webserver

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,31 @@
 package main
 
-import "github.com/marcosduarte-dev/go-api/configs"
+import (
+	"net/http"
+
+	"github.com/marcosduarte-dev/go-api/configs"
+	"github.com/marcosduarte-dev/go-api/internal/entity"
+	"github.com/marcosduarte-dev/go-api/internal/infra/database"
+	"github.com/marcosduarte-dev/go-api/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	println(config.DBDriver)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	ProductHandler := handlers.NewProductHandler(productDB)
+	
+	http.HandleFunc("/products", ProductHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
 }
